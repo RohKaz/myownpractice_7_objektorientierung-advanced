cc7db935eb6b16e0ef00ea83d5907a313db839bb
RohKaz <rohula-kazimi@hotmail.com>
2023-03-05 20:33:08 +0100

    Add classes BiologyStudent and ComputerScienceStudent

---
+++ src/main/java/org/example/ComputerScienceStudent.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ComputerScienceStudent implements Citizen{
+
+
+    @Override
+    public String getAdress() {
+        return null;
+    }
+
+    @Override
+    public int getIdentityCardNumber() {
+        return 0;
+    }
+}
+
